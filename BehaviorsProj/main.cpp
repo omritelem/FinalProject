d1124957a16edfd61213175949f32141e4917679
eliyakadar <eliyakadar@gmail.com>
2015-06-16 22:58:06 -0700

    jnf

--- BehaviorsProj/main.cpp
+++ BehaviorsProj/main.cpp
@@ -12,6 +12,7 @@
 #include "Map.h"
 #include "PathPlanner.h"
 #include "ConfigurationManager.h"
+#include <vector>
 
 using namespace std;
 
@@ -26,38 +27,49 @@ int main()
 //	Manager manager(&robot, &plnOA);
 //	manager.run();
 
-	ConfigurationManager cm(CONFIGURATION_PATH);
+//	ConfigurationManager cm(CONFIGURATION_PATH);
+	ConfigurationManager cm("Config_test");
 
 	Map map;
 	map.thickenMap(cm.map_path, cm.robot_width);
 	map.createGrids(cm.map_path, cm.map_resolution, cm.grid_resolution);
 
-	cell_coordinate cl(5,5);
-	cell_coordinate cl1(1,1);
-	cell_coordinate cl2(2,2);
-	cell_coordinate cl3(3,3);
+//	printMatrix(map._original_grid);
+//	for (int var = 0; var < 10; var++) {
+//		cout << endl << "";
+//	}
+//	printMatrix(map._thickened_grid);
 
-	PathPlanner pathPlanner(map._original_grid ,cl , cl);
-//	grid , cm.target_x, cm.target_y, cm.start_x, cm.start_y);
+	// Running the a* algorithm on the thickened map
+	// calculating robot location on grid...
+	double resolution_relation = cm.grid_resolution / cm.map_resolution;
+	// Location is ROUNDED!!!
+	int start_x = cm.start_x / resolution_relation;
+	int start_y = cm.start_y / resolution_relation;
+	int end_x = cm.target_x / resolution_relation;
+	int end_y = cm.target_y / resolution_relation;
+	cell_coordinate start_point(start_x, start_y);
+	cell_coordinate end_point(end_x, end_y);
+	PathPlanner pathPlanner(map._thickened_grid, start_point, end_point);
 
-	printMatrix(map._original_grid);
-	for (int var = 0; var < 10; var++) {
-		cout << endl << "";
+	vector<cell_coordinate> ass_star_result = pathPlanner.astar();
+
+	cout << "THE RESULT IS: " << ass_star_result.size() << endl;
+	for (int i = 0; i < ass_star_result.size(); ++i) {
+		cout << ass_star_result[i].x_Coordinate << " " << ass_star_result[i].y_Coordinate << endl;
 	}
-	printMatrix(map._thickened_grid);
-
 
  	int i = 5;
 
- 	vector<cell_coordinate> bla (10);
- 	vector<cell_coordinate>::iterator it;
-
- 	it = bla.begin();
- 	it = bla.insert(it, cl);
-	it = bla.insert(it, cl1);
-
-	cout<<bla.size();
-	bla.erase(bla.begin()+1);
+// 	vector<cell_coordinate> bla (10);
+// 	vector<cell_coordinate>::iterator it;
+//
+// 	it = bla.begin();
+// 	it = bla.insert(it, cl);
+//	it = bla.insert(it, cl1);
+//
+//	cout<<bla.size();
+//	bla.erase(bla.begin()+1);
 
  	return 0;
 }
