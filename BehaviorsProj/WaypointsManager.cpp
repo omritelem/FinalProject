24e1c222b119c6b783f7afa210f75d0edb67b49b
eliyakadar <eliyakadar@gmail.com>
2015-06-16 22:47:19 -0700

    Add waypoints

--- BehaviorsProj/WaypointsManager.cpp
+++ BehaviorsProj/WaypointsManager.cpp
@@ -17,6 +17,7 @@ void WaypointsManager::build_way_point_vector(int num_of_cells)
 {
 	int counter;
 	double m = 0;
+	bool is_vertical = 0;
 
 	for (int i = 0; i < astar_path.size(); i++)
 	{
@@ -26,13 +27,37 @@ void WaypointsManager::build_way_point_vector(int num_of_cells)
 			wayPoint wp(astar_path[i].x_Coordinate, astar_path[i].y_Coordinate , 0);
 			wayPoints[wayPoints.size()] = wp;
 			counter = 0;
+
+			if (i != 0)
+			{
+				if(astar_path[i].x_Coordinate == astar_path[i-1].x_Coordinate)
+				{
+					is_vertical = 1;
+				}
+				else
+				{
+					is_vertical = 0;
+					m = ((astar_path[i].y_Coordinate - astar_path[i-1].y_Coordinate) / (astar_path[i].x_Coordinate - astar_path[i-1].x_Coordinate));
+				}
+			}
+		}
+		else if ((astar_path[i].x_Coordinate == astar_path[i-1].x_Coordinate))
+		{
+			if(is_vertical == 0)
+			{
+				is_vertical = 1;
+				wayPoint wp3(astar_path[i].x_Coordinate, astar_path[i].y_Coordinate , 0);
+				wayPoints[wayPoints.size()] = wp3;
+				counter = 0;
+			}
 		}
 		else if(i == 1)
 		{
 			m = ((astar_path[i].y_Coordinate - astar_path[i-1].y_Coordinate) / (astar_path[i].x_Coordinate - astar_path[i-1].x_Coordinate));
 		}
-		else if(m != ((astar_path[i].y_Coordinate - astar_path[i-1].y_Coordinate) / (astar_path[i].x_Coordinate - astar_path[i-1].x_Coordinate)))
+		else if((is_vertical == 1) || (m != ((astar_path[i].y_Coordinate - astar_path[i-1].y_Coordinate) / (astar_path[i].x_Coordinate - astar_path[i-1].x_Coordinate))))
 		{
+			is_vertical == 0;
 			m = (astar_path[i].y_Coordinate - astar_path[i-1].y_Coordinate) / (astar_path[i].x_Coordinate - astar_path[i-1].x_Coordinate);
 			wayPoint wp2(astar_path[i].x_Coordinate, astar_path[i].y_Coordinate , 0);
 			wayPoints[wayPoints.size()] = wp2;
@@ -41,6 +66,11 @@ void WaypointsManager::build_way_point_vector(int num_of_cells)
 
 		counter++;
 	}
+}
+
+double WaypointsManager::calc_yaw(double m, cell_coordinate cell_from, cell_coordinate cell_to, bool is_verticle)
+{
+
 }
 
 WaypointsManager::WaypointsManager() {
--- BehaviorsProj/WaypointsManager.h
+++ BehaviorsProj/WaypointsManager.h
@@ -18,11 +18,12 @@ class WaypointsManager {
 public:
 
 	vector<wayPoint> wayPoints;
+	void build_way_point_vector(int num_of_cells);
 
 private:
 	vector<cell_coordinate> astar_path;
 
-	void build_way_point_vector(int num_of_cells);
+	double calc_yaw(double m, cell_coordinate cell_from, cell_coordinate cell_to, bool is_verticle);
 	WaypointsManager();
 	WaypointsManager(vector<cell_coordinate> path);
 	virtual ~WaypointsManager();
